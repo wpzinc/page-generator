45ee0a2b0c8ce009a49e2ba0aca7f19e59233e50
Tim Carr <tim@n7studios.co.uk>
2023-02-04 11:48:26 +0000

    Updated build scripts and .distignore

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -3,3 +3,24 @@ php create-actions-filters-docs.php
 
 # Generate .pot file
 php -n $(which wp) i18n make-pot ../ ../languages/page-generator.pot
+
+# Build ZIP file, excluding non-Plugin files
+cd ..
+rm page-generator.zip
+zip -r page-generator.zip . \
+-x "*.scss" \
+-x "*.git*" \
+-x ".scripts/*" \
+-x "tests/*" \
+-x "vendor/*" \
+-x "*.distignore" \
+-x "*.env.*" \
+-x ".gitignore" \
+-x "*.md" \
+-x "*codeception.*" \
+-x "composer.json" \
+-x "composer.lock" \
+-x "config.codekit3" \
+-x "phpcs.tests.xml" \
+-x "phpcs.xml" \
+-x "*.DS_Store" \
